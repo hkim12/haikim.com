ecc32c94ae3ecd0194f72a504c3fe9e1c026ed5e
hkim12 <hkim12@mica.edu>
2017-05-01 13:10:37 -0400

    Copy overrr

---
+++ Portfolio website /main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+	$( "#show-behance-work" ).click(function() {
+	$( ".section-1, .triangle-tilt-wrapper, #show-behance-work" ).toggleClass( "show-behance-work" );
+	});
+});
